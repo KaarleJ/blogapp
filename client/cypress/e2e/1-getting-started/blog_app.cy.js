537180a99278f760d694f30d92ed2e33f0292de2
Kaarle Järvinen <kaarle46@gmail.com>
2023-03-28 14:37:02 +0300

    fixing

--- client/cypress/e2e/1-getting-started/blog_app.cy.js
+++ client/cypress/e2e/1-getting-started/blog_app.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:3003')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    cy.contains('Blogs')
   })
 })
